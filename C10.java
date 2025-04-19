7e88228e2325a065d66eae79cd083a1726b4c2ba
Legitakshay <165594814+Legitakshay@users.noreply.github.com>
2025-04-20 01:19:52 +0530

    Add files via upload

---
+++ C10.java
@@ -0,0 +1,33 @@
+package Java;
+
+public class C10 {
+    public static void main(String[] args) {
+        //      int year= 4526;
+        //    if(year % 100 == 0){
+        //century year
+        //      if(year )
+        // }
+
+        //}
+        //int units = 456;
+        //double amt = 0;
+        //int u100 = 420, u200 = 1020, u400 = 2620;
+        //if (units <= 100)
+        //  System.out.println(units * 4.2);
+        //else if(units<=200)
+        //  System.out.println(u100+(units-100)*6);
+        //else if(units<=400)
+        //  System.out.println(u200+(units-200)*8);
+        //else
+        //    System.out.println(u400+(units-4000*13));
+        int a = 9;
+        if (a % 2 == 0) {
+            System.out.println("Even");
+
+        }else
+            System.out.println("odd");
+
+    }
+
+
+}
