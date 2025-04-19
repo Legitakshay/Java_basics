9cdee044f4319d1bd93fb33079bdd7a2aa5808dd
Legitakshay <165594814+Legitakshay@users.noreply.github.com>
2025-04-20 01:12:30 +0530

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day7 {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        int a = 25;
+        int b = 10;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println();
+
+    }
+
+}
