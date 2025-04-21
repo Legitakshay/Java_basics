cccd20cb7d8719a8efd72db3acc9f60f6134282e
Akshay Barman <165594814+Legitakshay@users.noreply.github.com>
2025-04-21 16:18:48 +0530

    Add files via upload

---
+++ NestedLoop.java
@@ -0,0 +1,36 @@
+package Java;
+
+import java.text.Format;
+import java.util.Scanner;
+
+public class NestedLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+            // Scanner sc = new Scanner(System.in);
+            //int n = sc.nextInt();
+            //for (int i = 1; i <= n; i++) {
+            // for (int j = n; j >=i; j--) {
+            //  System.out.print("*");
+            //  }
+            //      System.out.println("*");
+
+//for (int i=1;i<=n-i;i++){
+            //System.out.print(" ");
+
+            //  }
+//for(int j=1; j<= i ; j++){
+            //  System.out.println();
+
+        }
+    }
+}
+
+
+//}
+
